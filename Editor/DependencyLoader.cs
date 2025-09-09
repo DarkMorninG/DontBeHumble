9b69f64a8baf02982334dfb3f3b53fdad22c4f0e
DarkMorninG <Bustercall@gmx.net>
2025-09-09 21:38:54 +0200

    added dependencyLoader for git dependencies

---
+++ Editor/DependencyLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using Vault;
+
+namespace DBH.Editor {
+    [InitializeOnLoad]
+    public class DependencyLoader {
+        static DependencyLoader() {
+            new DependencyLoader().OnUnityStart();
+            Debug.Log("Git DependencyLoader Started");
+        }
+        public void OnUnityStart() {
+            Events.registeredPackages += EventsOnregisteredPackages;
+        }
+
+        private void EventsOnregisteredPackages(PackageRegistrationEventArgs packageRegistrationEventArgs) {
+            foreach (var packageInfo in packageRegistrationEventArgs.added) {
+                PullDependency(packageInfo.dependencies);
+            }
+        }
+
+        private void PullDependency(IEnumerable<DependencyInfo> dependencyInfos) {
+            var packageCollection = Client.List()
+                .Result;
+            var githubDependencies = dependencyInfos
+                .Where(info => info.version.Contains("github.com"))
+                .ToList();
+            packageCollection.Where(info => !githubDependencies.Select(dependencyInfo => dependencyInfo.name).Contains(info.name))
+                .ForEach(info => {
+                    Debug.Log("Found Missing Dependency " + info.version);
+                    Client.Add(info.version);
+                });
+        }
+    }
+}
